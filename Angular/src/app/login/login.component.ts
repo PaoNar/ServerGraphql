17f7904636903d709acefeee782fe43a20746509
Omar Cedeño <grarogccee@gmail.com>
2020-07-15 16:32:35 -0500

    loginof

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,68 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import Swal from 'sweetalert2';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { PermisosService } from '../servicios/permisos.service'
-import { Datarx } from '../models/datarx'
-import { LoginService } from '../servicios/login.service'
+import { PermisosService } from '../servicios/permisos.service';
+import { Datarx } from '../models/datarx';
+import { Apollo } from 'apollo-angular';
+import { Subscription } from 'rxjs';
+import gql from 'graphql-tag';
+
+const logear = gql`
+  query login($email: String, $password: String) {
+    login(email: $email, password: $password)
+  }
+`;
 // import { read } from 'fs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginData: FormGroup;
-  constructor(private router: Router,
-    private loginServices:LoginService,
+  private _querySub: Subscription;
+  constructor(
+    private router: Router,
     private permisos: PermisosService,
-    private formBuilder: FormBuilder) { }
+    private apollo: Apollo,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.loginData = this.formBuilder.group({
-      email:["admi@gmail.com",Validators.required],
-      password:["1",Validators.required],
-  }); 
-  }
-
-login():void{
-  let email =this.loginData.get('email').value;
-  let password =this.loginData.get('password').value;
-  let datalogin = {
-    data:{
-      password,
-      email
-    }
-  };
-    this.loginServices.login(datalogin).subscribe((data:Datarx)=>{
-  if(data.transaccion){
-    if(this.permisos.decodificarToken(data.token)){
-      console.log(data.data[0].rol)
-      sessionStorage.setItem('rol', JSON.stringify(data.data[0].rol));
-      this.router.navigate(['/menu']);
-    }else{
-     email='';
-      password='';
-      const Toast = Swal.fire({
-        position: 'top-right',
-        icon:'error',
-        title:`${data.msg}`,
-        showConfirmButton: false,
-        timer: 3000
-      });
-    }
-  } error=>{
-    email='';
-      password='';
-    const Toast = Swal.fire({
-      position: 'top-right',
-      icon:'error',
-      title:`${error}`,
-      showConfirmButton: false,
-      timer: 3000
+      email: ['admi@gmail.com', Validators.required],
+      password: ['1', Validators.required],
     });
-  };
-});
-}
-  
+  }
+  ngOnDestroy() {
+    this._querySub.unsubscribe();
+  }
+  login() {
+    let email = this.loginData.get('email').value;
+    let password = this.loginData.get('password').value;
+    this._querySub = this.apollo
+      .watchQuery<any>({
+        query: logear,
+        variables: {
+          email,
+          password,
+        },
+      })
+      .valueChanges.subscribe(({ data }) => {
+        if (data.login !== null) {
+          this.loginData = data;
+          this.permisos.decodificarToken(data.login.token);
+          console.log(this.permisos.decodificarToken(data.login.token))
+          this.router.navigate(['/menu']);
+        } else {
+          email = '';
+          password = '';
+          const Toast = Swal.fire({
+            position: 'top-right',
+            icon: 'error',
+            title: `${data.msg}`,
+            showConfirmButton: false,
+            timer: 3000,
+          });
+        }
+      });
+  }
 }
